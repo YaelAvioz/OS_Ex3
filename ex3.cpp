96e1f887702606fc45cc4df02b65069d25c42a69
chena <chena@hailo.ai>
2022-06-17 22:34:31 +0300

    added quques methos implementations

--- ex3.cpp
+++ ex3.cpp
@@ -1,9 +1,30 @@
 #include "ex3.h"
 
 void UnboundedQueue::enqueue(std::string news) {
-    mtx.lock();
-    queue.push(news);
-    mtx.unlock();
-    sem_post(&full);
+  m_mtx.lock();
+  m_queue.push(news);
+  m_mtx.unlock();
+  sem_post(&m_full);
 }
 
+std::string UnboundedQueue::dequque() {
+  sem_wait(&m_full);
+  m_mtx.lock();
+  auto news = m_queue.front();
+  m_queue.pop();
+  m_mtx.unlock();
+  return news;
+}
+
+std::string UnboundedQueue ::front() { return m_queue.front(); }
+
+void BoundedQueue::enqueue(std::string news) {
+  sem_wait(&m_empty);
+  UnboundedQueue::enqueue(news);
+}
+
+std::string BoundedQueue::dequque() {
+  auto news = UnboundedQueue::dequque();
+  sem_post(&m_empty);
+  return news;
+}
--- ex3.h
+++ ex3.h
@@ -6,7 +6,33 @@
 class UnboundedQueue {
 public:
   // constractor
-  UnboundedQueue() { sem_init(&full, 0, 0); };
+  UnboundedQueue() { sem_init(&m_full, 0, 0); };
+
+  // insert string to the buffer
+  void enqueue(std::string news);
+
+  // remove string from the buffer
+  std::string dequque();
+
+   std::string front();
+
+
+  // TODO: destructor
+  virtual ~UnboundedQueue() {
+    sem_destroy(&m_full);
+    delete (this);
+  };
+
+private:
+  sem_t m_full;
+  std::mutex m_mtx;
+  std::queue<std::string> m_queue;
+};
+
+class BoundedQueue : UnboundedQueue {
+public:
+  // constractor
+  BoundedQueue(int size) : UnboundedQueue() { sem_init(&m_empty, 0, size); };
 
   // insert string to the buffer
   void enqueue(std::string news);
@@ -15,25 +41,8 @@ public:
   std::string dequque();
 
   // TODO: destructor
-  virtual ~UnboundedQueue() {
-    sem_destroy(&full);
-    delete (this);
-  };
+  ~BoundedQueue() { sem_destroy(&m_empty); };
 
 private:
-  sem_t full;
-  std::mutex mtx;
-  std::queue<std::string> queue;
-};
-
-class BoundedQueue : UnboundedQueue {
-public:
-  // constractor
-  BoundedQueue(int size) : UnboundedQueue() { sem_init(&empty, 0, size); };
-
-  // TODO: destructor
-  ~BoundedQueue() { sem_destroy(&empty); };
-
-private:
-  sem_t empty;
+  sem_t m_empty;
 };
